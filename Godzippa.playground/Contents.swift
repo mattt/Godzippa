9441011664559128356afd326a5e63e41d6cf034
M.Gökay Borulday <m.gokay@borulday.com>
2018-09-04 18:54:32 +0300

    Use 'gzipDecompressed()' method to decompress (#15)

--- Godzippa.playground/Contents.swift
+++ Godzippa.playground/Contents.swift
@@ -4,7 +4,7 @@ import Godzippa
 let originalString = "Look out! It's Godzippa!"
 let originalData = originalString.data(using: .utf8)! as NSData
 let compressedData = try! originalData.gzipCompressed() as NSData
-let decompressedData = try! compressedData.gzipCompressed()
+let decompressedData = try! compressedData.gzipDecompressed()
 let decompressedString = String(data: decompressedData, encoding: .utf8)
 
 originalString == decompressedString
